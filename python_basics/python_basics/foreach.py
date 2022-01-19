8995ad785c55b962a62ce117eed9248b238585ca
Hoang To <hoang.to94@gmail.com>
2022-01-19 14:15:07 -0800

    python tutorial

---
+++ python_basics/python_basics/foreach.py
@@ -0,0 +1,25 @@
+# This is what a comment looks like
+fruits = ['apples', 'oranges', 'pears', 'bananas']
+for fruit in fruits:
+    print(fruit + ' for sale')
+
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75}
+for fruit, price in fruitPrices.items():
+    if price < 2.00:
+        print('%s cost %f a pound' % (fruit, price))
+    else:
+        print(fruit + ' are too expensive!')
+
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75}
+
+def buyFruit(fruit, numPounds):
+    if fruit not in fruitPrices:
+        print("Sorry we don't have %s" % (fruit))
+    else:
+        cost = fruitPrices[fruit] * numPounds
+        print("That'll be %f please" % (cost))
+
+# Main Function
+if __name__ == '__main__':
+    buyFruit('apples', 2.4)
+    buyFruit('coconuts', 2)
---
+++ python_basics/python_basics/person_classTest.py
@@ -0,0 +1,3 @@
+import person_class
+p1 = person_class.Person(12)
+print(p1.get_population())
